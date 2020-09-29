592890a08552661cdc9ef6db578fe1ee42c358a8
Gabriel Menezes da Silva <gabrielmenezes@MacBook-Pro-de-Gabriel.local>
2020-09-29 09:44:56 -0300

    Setup do appium

---
+++ features/steps_definition/appium.js
@@ -0,0 +1,53 @@
+const wd = require('wd');
+
+const PORT = 4723;
+
+const config = {
+  platformName: 'Android',
+  deviceName: 'Nexus_One_API_29',
+  app: 'android/app/build/outputs/apk/release/app-release.apk', // relative to root of project
+  appPackage: 'com.bitrisetestproject',
+  appActivity: 'com.bitrisetestproject.MainActivity',
+  automationName: 'uiautomator2',
+};
+// const config = {
+//   platformName: 'iOS',
+//   deviceName: 'iPhone SE (2nd generation)',
+//   platformVersion: '13.5',
+//   app:
+//     'ios/build/Build/Products/Release-iphonesimulator/BitriseTestProject.app', // relative to root of project
+// };
+const driver = wd.promiseChainRemote('localhost', PORT);
+beforeAll(async () => {
+  await driver.init(config);
+  await driver.sleep(20000);
+}); // Sometime for the app to load
+
+afterAll(async () => {
+  await driver.quit();
+});
+test('my first appium test', async () => {
+  expect(await driver.hasElementByAccessibilityId('welcome-message')).toBe(
+    true,
+  );
+});
+
+// Given('I am in app home page', {timeout: 30000}, async () => {
+//   let isWelcomeMessage = await driver.hasElementByAccessibilityId(
+//     'welcome-message',
+//   );
+//   assert.equal(isWelcomeMessage, true);
+// });
+
+// When('I click on "Pressione" button', async () => {
+//   let loginButton = await driver.elementByAccessibilityId('press-button');
+//   loginButton.click();
+// });
+
+// Then('I see the alert', {timeout: 2000}, async () => {
+//   await driver.setImplicitWaitTimeout(1500);
+//   let isAlert = await driver.hasElementByXPath(
+//     "//*[@text='Você apertou o botão']",
+//   );
+//   assert.equal(isAlert, true);
+// });
