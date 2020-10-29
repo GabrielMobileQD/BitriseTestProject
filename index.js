a412dee521d04f5d9f4ea37204d0eb4a3b4866e2
Gabriel Menezes da Silva <gabrielmenezes@MacBook-Pro-de-Gabriel.local>
2020-10-29 10:33:00 -0300

    Foi adicionado o codepush no android

--- index.js
+++ index.js
@@ -5,5 +5,10 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import codePush from 'react-native-code-push'
 
-AppRegistry.registerComponent(appName, () => App);
+const root = codePush({
+	checkFrequency: codePush.CheckFrequency.ON_APP_START,
+	installMode: codePush.InstallMode.IMMEDIATE,
+})(App)
+AppRegistry.registerComponent(appName, () => root);
